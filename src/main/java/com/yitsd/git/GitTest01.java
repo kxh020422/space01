52aa2e32b39e2b1f85ea0d1d2a5708ff3faa5b10
kxh <kxh020422@163.com>
2022-07-12 23:48:05 +0800

    idea-first-commit

---
+++ src/main/java/com/yitsd/git/GitTest01.java
@@ -0,0 +1,11 @@
+package com.yitsd.git;
+
+/**
+ * @ClassName GitTest01
+ * @Date 2022/7/12 23:42
+ */
+public class GitTest01 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
